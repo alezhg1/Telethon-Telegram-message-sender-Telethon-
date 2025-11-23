2434bd23fac89c77dacaa6d863b4948235bbd0b8
Az8n <146655473+alezhg1@users.noreply.github.com>
2025-11-23 20:52:19 +0300

    Update config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+#read README to know how to get API_ID and API_HASH, PHONE_NUMBER is your phone number
+API_ID = 'xxxxxx'
+API_HASH = 'xxxxxxxx'
+PHONE_NUMBER = '+xxxxxx'
+SESSION_FILE = 'sender_bot.session'
